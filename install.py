ff7d7465d2a27eccfcd8c0f4f9b2be4fbe92dfce
jason-bowen-zheng <18917572736@wo.cn>
2021-03-29 20:48:43 +0800

    2021-03-29

--- install.py
+++ install.py
@@ -6,20 +6,10 @@ from sys import platform
 
 VERSION = '0.3.1'
 
-def search_mcpy():
-    # 寻找文件存储位置
-    _os = __import__('os')
-    environ, path = _os.environ, _os.path
-    if 'MCPYPATH' in environ:
-        MCPYPATH = environ['MCPYPATH']
-    elif platform.startswith('win'):
-        MCPYPATH = path.join(path.expanduser('~'), 'mcpy')
-    else:
-        MCPYPATH = path.join(path.expanduser('~'), '.mcpy')
-    return MCPYPATH
-
 def install():
     # 安装
+    print('This is server %s installation script.')
+    input('Press ENTER to install server: ')
     file = path.join(search_mcpy(), 'server')
     data = {
             'long-desc': 'description.long',
@@ -28,6 +18,20 @@ def install():
             'port': 16384
             }
     dump(data, open(path.join(search_mcpy(), 'server.json'), 'w+'), indent='\t')
+
+def search_mcpy():
+    # 寻找文件存储位置
+    _os = __import__('os')
+    environ, path = _os.environ, _os.path
+    if 'MCPYPATH' in environ:
+        MCPYPATH = environ['MCPYPATH']
+    elif platform == 'darwin':
+        MCPYPATH = path.join(path.expanduser('~'), 'Library', 'Application Support', 'mcpy')
+    elif platform.startswith('win'):
+        MCPYPATH = path.join(path.expanduser('~'), 'mcpy')
+    else:
+        MCPYPATH = path.join(path.expanduser('~'), '.mcpy')
+    return MCPYPATH
 
 if __name__ == '__main__':
     install()
